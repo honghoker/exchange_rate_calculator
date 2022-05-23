fc73d8c640a67e4ea92a7027804b02faa890732c
honghoker <ekflzhftk@naver.com>
2022-05-23 18:48:03 +0900

    docs : firebase setting

--- exchange_rate_calculator/exchange_rate_calculator/exchange_rate_calculatorApp.swift
+++ exchange_rate_calculator/exchange_rate_calculator/exchange_rate_calculatorApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct exchange_rate_calculatorApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
