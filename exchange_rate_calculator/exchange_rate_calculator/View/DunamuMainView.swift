589cf6024893c7ed08936badf2f2a66d9e25485d
honghoker <ekflzhftk@naver.com>
2022-06-21 13:15:09 +0900

    Feat: 기준통화 추가, 팝업 SPM 추가

--- exchange_rate_calculator/exchange_rate_calculator/View/DunamuMainView.swift
+++ exchange_rate_calculator/exchange_rate_calculator/View/DunamuMainView.swift
@@ -10,6 +10,7 @@ import FlagKit
 import Introspect
 import UIKit
 import RealmSwift
+import PopupView
 
 class EditHelper: ObservableObject {
     @Published var currencyEdit: Bool = false
@@ -44,50 +45,207 @@ struct DunamuMainView: View {
     @ObservedObject var dunamuViewModel = DunamuViewModel()
     @ObservedObject var editHelper = EditHelper()
     @ObservedObject var myCountryList = BindableResults(results: try! Realm().objects(MyCountryModel.self))
+    @State var showModal = false
+    
+    let refreshControlHelper = RefreshControlHelper()
     
     init() {
         print("@@@@@@ realm URL : \(Realm.Configuration.defaultConfiguration.fileURL!)" )
     }
     
-    let refreshControlHelper = RefreshControlHelper()
+    @ViewBuilder func popList() -> some View {
+        GeometryReader { _ in
+            VStack() {
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack {
+                            ForEach(Array(countryModelList.keys), id: \.self) { value in
+                                ZStack(alignment: .trailing) {
+                                    HStack {
+    //                                    Image(uiImage: Flag(countryCode: "KRW")!.originalImage)
+    //                                        .resizable()
+    //                                        .clipShape(Circle())
+    //                                        .scaledToFit()
+    //                                        .frame(width: 40, height: 40)
+                                        
+                                        VStack(alignment: .leading) {
+                                            Text("KRW")
+                                                .fontWeight(.semibold)
+                                            Text("대한민국 원")
+                                                .fontWeight(.semibold)
+
+                                            Divider()
+                                        }
+                                    }
+
+                                    Image(systemName: "arrow.right")
+                                        .foregroundColor(.gray)
+
+                                }
+
+                            }
+                    } // VStack
+                    .padding()
+                } // ScrollView
+                .background(Color.white)
+                .frame(width: UIScreen.main.bounds.width - 80, height: UIScreen.main.bounds.height - 250)
+                .cornerRadius(8)
+                
+                Button(action: {
+                    withAnimation {
+                        self.showModal.toggle()
+                    }
+                }, label: {
+                    Image(systemName: "xmark")
+                        .resizable()
+                        .frame(width: 15, height: 15)
+                        .foregroundColor(.black)
+                        .padding(20)
+                })
+                .background(Color.white)
+                .clipShape(Circle())
+                .padding(.top, 25)
+            } // VStack
+        }
+        .background(Color.black.opacity(0.5).edgesIgnoringSafeArea(.all))
+    }
     
-    var body: some View {
+    @ViewBuilder func baseCurrency() -> some View {
+        let endIdx = "KRW".index("KRW".startIndex, offsetBy: 1)
+        let result = String("KRW"[...endIdx])
+        let flag = Flag(countryCode: result)
+        let originalImage = flag?.originalImage
+        
         VStack {
+            Divider()
+            HStack{
+                Text("현재 기준 통화")
+                    .fontWeight(.bold)
+                    .font(.custom("IBMPlexSansKR-Regular", size: 14))
+                    .foregroundColor(.black)
+                    .padding(.top, 12)
+                    .padding(.leading, 16)
+                Spacer()
+            }
             HStack(spacing: 0) {
-                Text("")
-                    .frame(maxWidth: .infinity)
-                if dunamuViewModel.dunamuModels.isEmpty == false {
-                    Text("\(dunamuViewModel.dunamuModels[1].time) 한국 기준")
+                if originalImage != nil {
+                    Spacer().frame(width: 16)
+                    Image(uiImage: originalImage!)
+                        .resizable()
+                        .clipShape(Circle())
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                }
+                Spacer().frame(width: 12)
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("KRW")
+                        .fontWeight(.bold)
                         .font(.custom("IBMPlexSansKR-Regular", size: 14))
                         .foregroundColor(.black)
+                    Text("\(countryModelList["KRW"]!.country) \(countryModelList["KRW"]!.currencyName)")
                         .fontWeight(.medium)
+                        .font(.custom("IBMPlexSansKR-Regular", size: 12))
+                        .foregroundColor(.gray)
                 }
-                Text(editHelper.currencyEdit ? "취소" : "편집")
+                Spacer()
+                
+                Button(action: {
+                    withAnimation {
+                        self.showModal.toggle()
+                    }
+                }, label: {
+                    Text("변경")
+                        .fontWeight(.medium)
+                        .font(.custom("IBMPlexSansKR-Regular", size: 12))
+                        .foregroundColor(.black)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 8)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.gray, lineWidth: 1)
+                        )
+                })
+                
+                Spacer().frame(width: 16)
+            }
+            Divider()
+        }
+    }
+    
+    @ViewBuilder func updateTimeAndEdit() -> some View {
+        HStack(spacing: 0) {
+            Text("")
+                .frame(maxWidth: .infinity)
+            if dunamuViewModel.dunamuModels.isEmpty == false {
+                Text("\(dunamuViewModel.dunamuModels[1].time) 한국 기준")
                     .font(.custom("IBMPlexSansKR-Regular", size: 14))
                     .foregroundColor(.black)
                     .fontWeight(.medium)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                    .onTapGesture {
-                        editHelper.toggleEdit()
-                    }
             }
-            .padding(.horizontal, 16)
-            .padding(.top, 12)
-            Spacer().frame(height : 8)
-            
-            Section(header: DunamuMainViewHeader()) {
-                List(dunamuViewModel.dunamuModels) { dunamu in
-                    DunamuRowView(dunamu, $editHelper.currencyEdit, $myCountryList.results)
-                        .listRowInsets(EdgeInsets())
-                        .listRowBackground(Color.clear)
-                } // List
-                .listStyle(.plain)
-                .introspectTableView {
-                    self.configureRefreshControl($0)
+            Text(editHelper.currencyEdit ? "취소" : "편집")
+                .font(.custom("IBMPlexSansKR-Regular", size: 14))
+                .foregroundColor(.black)
+                .fontWeight(.medium)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .onTapGesture {
+                    editHelper.toggleEdit()
                 }
+        }
+        .padding(.horizontal, 16)
+        .padding(.top, 12)
+        .padding(.bottom, 8)
+    }
+    
+    @ViewBuilder func currencyList() -> some View {
+        Section(header: DunamuMainViewHeader()) {
+            List(dunamuViewModel.dunamuModels) { dunamu in
+                DunamuRowView(dunamu, $editHelper.currencyEdit, $myCountryList.results)
+                    .listRowInsets(EdgeInsets())
+                    .listRowBackground(Color.clear)
+            } // List
+            .listStyle(.plain)
+            .introspectTableView {
+                self.configureRefreshControl($0)
             }
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            baseCurrency()
+            updateTimeAndEdit()
+            currencyList()
         } // VStack
         .padding(0)
+        .popup(isPresented: $showModal, type: .toast, position: .bottom, dragToDismiss: true) {
+            ForEach(Array(countryModelList.keys), id: \.self) { value in
+                ZStack(alignment: .trailing) {
+                    HStack {
+//                                    Image(uiImage: Flag(countryCode: "KRW")!.originalImage)
+//                                        .resizable()
+//                                        .clipShape(Circle())
+//                                        .scaledToFit()
+//                                        .frame(width: 40, height: 40)
+                        
+                        VStack(alignment: .leading) {
+                            Text("KRW")
+                                .fontWeight(.semibold)
+                            Text("대한민국 원")
+                                .fontWeight(.semibold)
+
+                            Divider()
+                        }
+                    }
+
+                    Image(systemName: "arrow.right")
+                        .foregroundColor(.gray)
+
+                }
+
+            }
+
+        }
     } // body
 }
 
